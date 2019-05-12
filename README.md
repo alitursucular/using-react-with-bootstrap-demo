4953ec397d7243f9667d6379c1b256ab79477610
Ali Turşucular <alitursucular@gmail.com>
2019-05-12 17:03:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,68 +1,8 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Using React with Bootstrap (react-bootstrap)
+This project is only about adding Bootstrap to React (integrating Bootstrap with React apps) and creating a basic grid for further projects. I will not be discussing about React basics like i did in my other posts [Fetching data from a RESTful API using Fetch API with React](https://alitursucular.github.io/fetching-api-data-with-reactjs/) and [ReactJS Login / Log Out toggle button using states](https://alitursucular.github.io/reactjs-login-log-out-toggle-button-using-states/)
 
-## Available Scripts
+For the **blog post** and the **demo** of this repo please visit: https://alitursucular.github.io/using-react-with-bootstrap/
 
-In the project directory, you can run:
+---
 
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Note: The source code is in the _master branch_ while, the build code is in _gh-pages_.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Alert variant="info">
+                    Hello there! This page is built with <strong>React</strong>, in respect to its original plain <Alert.Link href="https://getbootstrap.com/docs/4.1/examples/grid/" target="_blank">Bootstrap 4.1 version</Alert.Link>.
+                </Alert>
+                <h1>Bootstrap grid examples</h1>
+                <p className="lead">Basic grid layouts to get you familiar with building within the Bootstrap grid system.</p>
+
+                <h2>Five grid tiers</h2>
+                <p>There are five tiers to the Bootstrap grid system, one for each range of devices we support. Each tier starts at a minimum viewport size and automatically applies to the larger devices unless overridden.</p>
+
+                <Row>
+                    <Col xs={4}>.col-4</Col>
+                    <Col xs={4}>.col-4</Col>
+                    <Col xs={4}>.col-4</Col>
+                </Row>
+
+                <Row>
+                    <Col sm={4}>.col-sm-4</Col>
+                    <Col sm={4}>.col-sm-4</Col>
+                    <Col sm={4}>.col-sm-4</Col>
+                </Row>
+
+                <Row>
+                    <Col md={4}>.col-md-4</Col>
+                    <Col md={4}>.col-md-4</Col>
+                    <Col md={4}>.col-md-4</Col>
+                </Row>
+
+                <Row>
+                    <Col lg={4}>.col-lg-4</Col>
+                    <Col lg={4}>.col-lg-4</Col>
+                    <Col lg={4}>.col-lg-4</Col>
+                </Row>
+
+                <Row>
+                    <Col xl={4}>.col-xl-4</Col>
+                    <Col xl={4}>.col-xl-4</Col>
+                    <Col xl={4}>.col-xl-4</Col>
+                </Row>
+
+                <h2>Three equal columns</h2>
+                <p>Get three equal-width columns <strong>starting at desktops and scaling to large desktops</strong>. On mobile devices, tablets and below, the columns will automatically stack.</p>
+                <Row>
+                    <Col md={4}>.col-md-4</Col>
+                    <Col md={4}>.col-md-4</Col>
+                    <Col md={4}>.col-md-4</Col>
+                </Row>
+
+                <h2>Three unequal columns</h2>
+                <p>Get three columns <strong>starting at desktops and scaling to large desktops</strong> of various widths. Remember, grid columns should add up to twelve for a single horizontal block. More than that, and columns start stacking no matter the viewport.</p>
+                <Row>
+                    <Col md={3}>.col-md-3</Col>
+                    <Col md={6}>.col-md-6</Col>
+                    <Col md={3}>.col-md-3</Col>
+                </Row>
+
+                <h2>Two columns</h2>
+                <p>Get two columns <strong>starting at desktops and scaling to large desktops</strong>.</p>
+                <Row>
+                    <Col md={8}>.col-md-8</Col>
+                    <Col md={4}>.col-md-4</Col>
+                </Row>
+
+                <h2>Full width, single column</h2>
+                <p className="text-warning">No grid classes are necessary for full-width elements.</p>
+
+                <hr></hr>
+
+                <h2>Two columns with two nested columns</h2>
+                <p>Per the documentation, nesting is easy—just put a row of columns within an existing column. This gives you two columns <strong>starting at desktops and scaling to large desktops</strong>, with another two (equal widths) within the larger column.</p>
+                <p>At mobile device sizes, tablets and down, these columns and their nested columns will stack.</p>
+                <Row>
+                    <Col md={8}>
+                        .col-md-8
+                        <Row>
+                            <Col md={6}>.col-md-6</Col>
+                            <Col md={6}>.col-md-6</Col>
+                        </Row>
+                    </Col>
+                    <Col md={4}>.col-md-4</Col>
+                </Row>
+
+                <hr></hr>
+
+                <h2>Mixed: mobile and desktop</h2>
+                <p>The Bootstrap v4 grid system has five tiers of classes: xs (extra small), sm (small), md (medium), lg (large), and xl (extra large). You can use nearly any combination of these classes to create more dynamic and flexible layouts.</p>
+                <p>Each tier of classes scales up, meaning if you plan on setting the same widths for xs and sm, you only need to specify xs.</p>
+                <Row>
+                    <Col xs={12} md={8}>.col-12 .col-md-8</Col>
+                    <Col xs={6} md={4}>.col-6 .col-md-4</Col>
+                </Row>
+                <Row>
+                    <Col xs={6} md={4}>.col-6 .col-md-4</Col>
+                    <Col xs={6} md={4}>.col-6 .col-md-4</Col>
+                    <Col xs={6} md={4}>.col-6 .col-md-4</Col>
+                </Row>
+                <Row>
+                    <Col xs={6}>.col-6</Col>
+                    <Col xs={6}>.col-6</Col>
+                </Row>
+
+                <hr></hr>
+
+                <h2>Mixed: mobile, tablet, and desktop</h2>
+                <p></p>
+                <Row>
+                    <Col xs={12} sm={6} lg={8}>.col-12 .col-sm-6 .col-lg-8</Col>
+                    <Col xs={6} lg={4}>.col-6 .col-lg-4</Col>
+                </Row>
+                <Row>
+                    <Col xs={6} sm={4}>.col-6 .col-sm-4</Col>
+                    <Col xs={6} sm={4}>.col-6 .col-sm-4</Col>
+                    <Col xs={6} sm={4}>.col-6 .col-sm-4</Col>
+                </Row>
+
+            </Container>
+        </div>
+    );
 }
 
 export default App;
